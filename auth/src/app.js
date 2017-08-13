4fdbcd56374672a86e1bdfe6667df3ee62b7213f
Leo Lee <nxl1969@gmail.com>
2017-08-13 15:27:47 -0400

    Added components folder and app.js

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
       <View>
         <Text> An App!</Text>
