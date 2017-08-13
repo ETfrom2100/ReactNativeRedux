bc72061ab89cf15f4f41dbac351a498fb6616e6e
Leo Lee <nxl1969@gmail.com>
2017-08-13 12:51:03 -0400

    Added a button component

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => { //destructure props
   const { title, artist, thumbnail_image, image } = album; // I want those properties from the album
@@ -22,8 +23,13 @@ const AlbumDetail = ({ album }) => { //destructure props
             <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
          <Image source={{ uri: image }} style={imageStyle} />
+      </CardSection>
+
+      <CardSection>
+         <Button />
       </CardSection>
 
     </Card>
