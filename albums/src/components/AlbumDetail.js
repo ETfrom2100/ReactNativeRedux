8b65ca76a0f25c2aba174b46a6d42c499e288475
Leo Lee <nxl1969@gmail.com>
2017-08-13 12:07:42 -0400

    Added styles for thumbnail container and header text

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -5,15 +5,19 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => { //destructure props
   const { title, artist, thumbnail_image } = album; // I want those properties from the album
-  const {thumbnailStyle, headerContentStyle } = styles; // destructure properties from styles
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle } = styles; // destructure properties from styles
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
         </View>
         <View style={headerContentStyle}>
-            <Text>{title}</Text>
+            <Text style={headerTextStyle}>{title}</Text>
             <Text>{artist}</Text>
         </View>
       </CardSection>
@@ -28,9 +32,18 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     width: 50,
     height: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
   }
 };
 export default AlbumDetail;
