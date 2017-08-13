0ca5408980ade266bd16f1e3d0c508356ea4725b
Leo Lee <nxl1969@gmail.com>
2017-08-13 12:00:40 -0400

    Added styles for thumbnail image and destructured properties from styles

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -5,13 +5,14 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => { //destructure props
   const { title, artist, thumbnail_image } = album; // I want those properties from the album
+  const {thumbnailStyle, headerContentStyle } = styles; // destructure properties from styles
   return (
     <Card>
       <CardSection>
         <View>
-          <Image source={{ uri: thumbnail_image }} />
+          <Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
         </View>
-        <View style={styles.headerContentStyle}>
+        <View style={headerContentStyle}>
             <Text>{title}</Text>
             <Text>{artist}</Text>
         </View>
@@ -26,6 +27,10 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50
   }
 };
 export default AlbumDetail;
