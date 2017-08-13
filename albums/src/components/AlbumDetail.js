66fac6907829ce48f09b9d9dab21bdaf23be1d06
Leo Lee <nxl1969@gmail.com>
2017-08-13 11:52:54 -0400

    Destrutured props and made it easier to reference properties

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => { //destructure props
+  const { title, artist, thumbnail_image } = album; // I want those properties from the album
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View>
+          <Image source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={styles.headerContentStyle}>
+            <Text>{title}</Text>
+            <Text>{artist}</Text>
+        </View>
       </CardSection>
 
-      
+
     </Card>
   );
 };
 
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  }
+};
 export default AlbumDetail;
