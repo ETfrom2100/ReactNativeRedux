e35a7a0bc5539724a28ab4f3290fe36fc9c93c54
Leo Lee <nxl1969@gmail.com>
2017-08-14 23:08:48 -0400

    Create a user if he failed to sign in, providing the email is not in use

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import { View } from 'react-native';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common/';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
   onButtonPress() {
     const { email, password } = this.state;
-    firebase.auth().signInWithEmailAndPassword(email, password);
+    this.setState({ error: '' });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(() => {
+              this.setState({ error: 'Authentication failed' });
+        });
+    });
   }
   render() {
     return (
       <Card>
         <CardSection>
           <Input
+          autoCapitalize='none'
           placeholder="user@gmail.com"
           label="Email"
           value={this.state.email}
@@ -23,6 +31,7 @@ class LoginForm extends Component {
         </CardSection>
         <CardSection>
           <Input
+
             secureTextEntry
             placeholder="password"
             label="Password"
@@ -30,6 +39,9 @@ class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Log In
@@ -39,5 +51,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
--- auth/src/components/common/Input.js
+++ auth/src/components/common/Input.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry, autoCapitalize }) => {
     const { inputStyle, labelStyle, containerStyle } = styles;
     return (
       <View style={containerStyle}>
         <Text style={labelStyle}>{label}</Text>
         <TextInput
+          autoCapitalize={autoCapitalize}
           placeholder={placeholder}
           autoCorrect={false}
           style={inputStyle}
