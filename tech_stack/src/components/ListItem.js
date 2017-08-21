f7004915b6ba22d5a45b5d0416e0df6157beb861
Leo Lee <nxl1969@gmail.com>
2017-08-21 00:24:41 -0400

    Added styles to the ListItem component.

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
-  render(){
+  render() {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library;
     return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
-    )
+      <TouchableWithoutFeedback
+       onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
+    );
   }
 }
 
@@ -20,5 +29,5 @@ const styles = {
     fontSize: 18,
     paddingLeft: 15
   }
-}
-export default ListItem;
+};
+export default connect(null, actions)(ListItem);
