97b27d39d86f04a39bdc1d13c3c4f5608b5cada1
Leo Lee <nxl1969@gmail.com>
2017-09-09 23:08:24 -0400

    Save data to firebase

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,7 +1,9 @@
+import firebase from 'firebase';
 import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE
 } from './types';
+
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -11,5 +13,8 @@ export const employeeUpdate = ({ prop, value }) => {
 };
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log(name, phone, shift);
+  //console.log(name, phone, shift);
+  const { currentUser }  = firebase.auth();
+  firebase.database().ref(`/users/${currentUser.uid}/employees`)
+  .push({ name, phone, shift });
 };
